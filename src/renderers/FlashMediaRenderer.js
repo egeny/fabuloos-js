fd15837b6d8b73250d380d7eb0605c02c4ec835a
neovov <neovov@gmail.com>
2012-11-23 17:29:24 +1000

    Defining a static variable on FlashMediaRenderer class to ease the plugin's file URL change

--- src/renderers/FlashMediaRenderer.js
+++ src/renderers/FlashMediaRenderer.js
@@ -42,6 +42,9 @@
 	FlashMediaRenderer.prototype = new Renderer(); // Inherit from Renderer
 	FlashMediaRenderer.prototype.constructor = FlashMediaRenderer; // Don't forget to correct the constructor
 
+	// The plugin file to use (this var is used to easily set the file URL)
+	FlashMediaRenderer.file = "FlashMediaRenderer.swf";
+
 
 	/**
 	 * Default plugin configuration
@@ -52,7 +55,7 @@
 		width:  720,
 		height: 405,
 		type: "application/x-shockwave-flash",
-		data: "FlashMediaRenderer.swf",
+		data: FlashMediaRenderer.file,
 		params: {
 			allowscriptaccess: "always",
 			wmode: "transparent"
