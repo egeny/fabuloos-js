4a885e75748142c956980d72dedcf6ff4f7882c3
neovov <neovov@gmail.com>
2013-01-09 19:30:15 +1000

    Rewriting the grunt file

--- grunt.js
+++ grunt.js
@@ -4,53 +4,57 @@ module.exports = function(grunt) {
 	// Use JavaScript script mode
 	"use strict";
 
-	var name = 'fabuloos';
-
 	// Project configuration.
 	grunt.initConfig({
 		meta: {
-			name: 'fabuloos',
-			version: '1.0'
+			name: "fabuloos",
+			version: "1.0"
 		},
+
+		// Lint task
 		lint: {
-			files: ['grunt.js', '<config:concat.dist.src>', 'test/*.js']
+			files: ["src/**/*.js", "test/*.js"]
 		},
-		qunit: {
-			files: ['test/**/*.html']
-		},
+
+		// Concatenation task
 		concat: {
-			dist: {
-				src: [
-					'src/renderers/Renderer.js',
-					'src/renderers/HTMLMediaRenderer.js',
-					'src/renderers/FlashRenderer.js',
-					'src/renderers/FlashMediaRenderer.js',
-					'src/renderers/YoutubeMediaRenderer.js',
+			src: {
+				renderers: [
+					"src/renderers/Renderer.js",
 
-					'src/core.js',
-					'src/event.js',
-					'src/api.js',
-
-					'src/tracks.js',
-					'src/plugins/tracks.js',
-
-					'src/plugins/playlist.js',
-					'src/plugins/tracker.js',
-					'src/plugins/ads.js'
+					"src/renderers/HTMLMediaRenderer.js",
+					"src/renderers/FlashRenderer.js",
+					"src/renderers/FlashMediaRenderer.js"
 				],
-				dest: 'build/' + name + '.js'
-			}
+				core: [
+					"src/core.js",
+					"src/event.js",
+					"src/api.js",
+					"src/tracks.js"
+				],
+				plugins: [
+					"src/plugins/tracks.js",
+					"src/plugins/playlist.js",
+					"src/plugins/tracker.js",
+					"src/plugins/ads.js"
+				]
+			},
+
+			dest: "build/<%= meta.name %>-<%= meta.version %>.js"
 		},
+
+		// Minification task
 		min: {
-			dist: {
-				src: ['build/' + name + '.js'],
-				dest: 'build/' + name + '.min.js'
-			}
+			src:  "build/<%= meta.name %>-<%= meta.version %>.js",
+			dest: "build/<%= meta.name %>-<%= meta.version %>.min.js"
 		},
-		watch: {
-			files: '<config:lint.files>',
-			tasks: 'lint qunit'
+
+		// QUnit task
+		qunit: {
+			files: ["test/**/*.html"]
 		},
+
+		// JSHint options
 		jshint: {
 			options: {
 				curly:     true,
@@ -62,28 +66,55 @@ module.exports = function(grunt) {
 				sub:       false,
 				validthis: true,
 				browser:   true
-			},
-			globals: {}
-		},
-		uglify: {}
+			}
+		}
 	});
 
-	// Default task.
-	grunt.registerTask('default', 'lint concat min');
-	grunt.registerTask('concat', "Concatenate files, replace @VERSION with meta.version value", function() {
+
+	// Registering tasks
+	grunt.registerTask("default", "lint concat min");
+
+	grunt.registerTask("concat", "Concatenate files, replace @VERSION with meta.version value", function() {
 		var
-			config   = grunt.config('concat.dist'),
-			files    = grunt.file.expandFiles(config.src),
-			compiled = grunt.helper('concat', files);
+			// Get the files list
+			files = grunt.config( "concat" ).src,
+
+			// Prepare the output
+			compiled, dest = grunt.config.process( "concat" ).dest;
+
+		// Update the files list to fix unexisting files
+		files = grunt.file.expandFiles( [].concat( files.renderers, files.core, files.plugins ) );
+
+		// Compile the files
+		compiled = grunt.helper( "concat", files );
 
 		// Replace @VERSION with meta.version value
-		compiled = compiled.replace(/@VERSION/g, grunt.config('meta.version'));
+		compiled = compiled.replace( /@VERSION/g, grunt.config( "meta.version" ) );
 
 		// Write the file
-		grunt.file.write(config.dest, compiled);
+		grunt.file.write( dest, compiled );
 
 		// Log
-		grunt.log.writeln('File "' + config.dest + '" created.');
+		grunt.log.writeln( 'File "' + dest + '" created.' );
+	});
+
+	grunt.registerTask("min", "Minify source file", function() {
+		var
+			config = grunt.config.process( "min" ),
+			max = grunt.file.read( config.src ),
+			min = grunt.helper( "uglify", max, grunt.config( "uglify" ) );
+
+		// Write the file
+		grunt.file.write( config.dest, min );
+
+		// Fail task if errors were logged.
+		if (this.errorCount) { return false; }
+
+		// Otherwise, print a success message....
+		grunt.log.writeln( 'File "' + config.dest + '" created.');
+
+		// ...and report some size information.
+		grunt.helper( "min_max_info", min, max );
 	});
 
 };
