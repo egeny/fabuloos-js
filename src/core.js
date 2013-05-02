5e9bbb7d38bbf9a948ba30ed0b7258daa39c0b82
neovov <neovov@gmail.com>
2013-05-03 04:45:34 +1000

    Light refactoring of the core
    - Finally defining the signatures of the main function and how to handle the element to enhance
    - Proper caching (no fake array)
    - Clearer prototype extending. Now Fab.extend will modify the prototype and not create a new one and use some tricks to work with inheritance. The goal is 0 prototype chain.
    - Defining nicely what the init() method should do. Adding an unique property for "private" stuff, keeping the instance clean
    - Adding the destroy() method (will restore and remove from cache)
    - Adding the element() method to get the current element (renderer node), or set the element to replace with a renderer
    - Adding the restore() method to restore the replaced element

--- src/core.js
+++ src/core.js
@@ -1,141 +1,256 @@
 /*global Renderer */
 
+// Expose
+window.fabuloos = window.fab = fab;
+window.Fabuloos = window.Fab = Fab;
+
 /**
- * A simple function handling the instances cache (create a new if needed)
+ * The fabuloos function
+ * Use it to create a new fabuloos player or to get an existing one from the instances' cache.
  *
- * @param {string} id The ID attribute of the source &lt;video&gt; or &lt;audio&gt; to enhance
- * @param {object} [config={}] The player configuration
+ * @param {string} id The ID attribute of the element to enhance (might be `<audio>`, `<video>` or any element).
+ * @return {fabuloos} Return a new fabuloos instance or an instance from the instances' cache.
  *
- * @returns {fabuloos} Return a player instance or false if the element doesn't exists
+ * @param {Element} element The element to enhance (might be `<audio>`, `<video>` or any element).
+ * @return {fabuloos} Return a new fabuloos instance or an instance from the instances' cache.
+ *
+ * @param {object} config The configuration to apply.
+ *   If the configuration contains an `element` property the player will be based on this element.
+ *   You can specify a string or an Element, just as the previous signatures.
+ * @return {fabuloos} Return a new fabuloos instance or an instance from the instances' cache.
+ *
+ * @param {undefined}
+ * @return {fabuloos} Return a new fabuloos instance.
  */
-function fab( id, config ) {
-	if (!fab.instances[id]) {
-		fab.instances[id] = new fab.prototype.init( id, config );
+function fab(config) {
+	var
+		// First, find the element (might be { element: "div" } or "div")
+		element = config ? config.element || config : {},
+
+		// Then find the id
+		// It might be a string (simply use it after removing the debuting hash)
+		// Or an Element, simply try to retrieve its id
+		id = typeof element === "string" ? element.replace("#", "") : element.id,
+
+		// Loop specific
+		i = 0, instance;
+
+	// Search for an instance in the cache having this id
+	while (id && (instance = Fab.instances[i++])) {
+		if (instance._.id === id) {
+			return instance;
+		}
 	}
 
-	return fab.instances[id];
-}
+	// No instance found, create a new one
+	return new Fab(config);
+} // end of fab()
 
 
 /**
- * An expando to be used in the DOM
- * @static
- * @type string
+ * An unique identifier for this class.
+ * Used to link a DOM element to a JS object.
+ * @api dev
+ * TODO: XXX/MOVE
  */
 fab.expando = "fabuloos" + (+new Date());
-
-
-// Is the class currently extending itself? Used to prevent code execution on init method during extending
-fab.extending = false;
-
-
-/**
- * A cache for all fabuloos' instances
- * @static
- * @type object
- */
-fab.instances = {};
 
 
 /**
  * A collection of regexp used to split and trim
  * @static
  * @type RegExp
+ * TODO: XXX/MOVE
  */
 fab.rSplit = /\s+/;
 fab.rTrim  = /^\s+|\s+$/g;
 
 
 /**
- * The current script's version
- * @see http://semver.org
- *
- * @static
- * @type string
+ * The current script's version (http://semver.org)
+ * @type {string}
+ * TODO: Fab should have access too
  */
 fab.version = "@VERSION";
 
 
-fab.prototype = {
+/**
+ * The base Fabuloos class
+ * @see #fab() for signatures
+ */
+function Fab(config) {
+	this.init(config);
+} // end of Fab()
 
+
+/**
+ * A cache for all fabuloos' instances
+ * @api dev
+ */
+Fab.instances = [];
+
+
+/**
+ * A RegExp used to detect the presence of "_super" in a function's content
+ * This RegExp will be used to check if we have to create a facade for a method when inheriting
+ * @type {RegExp}
+ */
+var rSuper = /xyz/.test(function() { "xyz"; }) ? /\b_super\b/ : /.*/;
+
+
+/**
+ * Create a facade function to simulate inheritance
+ * This closure will create this._super and call the wanted method.
+ *
+ * @param {function} fn The new function to call
+ * @param {*} _super The super value (might be of any type)
+ * @return {function} Return a facade function for a specific function
+ */
+function createFacade(fn, _super) {
+	return function facade() {
+		// Define the _super property (allow this._super inside the method)
+		this._super = _super;
+
+		// Launch the method passing the right this and the arguments, store the result for returning
+		var result = fn.apply(this, arguments);
+
+		// Delete the _super since we don't need it anymore
+		delete this._super;
+
+		// Return the method's result
+		return result;
+	};
+} // end of createFacade()
+
+
+/**
+ * Extend the Fabuloos' prototype with a given object literal.
+ * It will simulate inheritance by giving access to this._super.
+ * @api dev
+ *
+ * @param {object} obj The object literal to merge to the prototype
+ * @return {undefined} Return nothing.
+ *
+ * @example
+ *   Fab.extend({
+ *     play: function() { console.log("First play"); }
+ *   });
+ *
+ *   Fab.extend({
+ *     play: function() {
+ *       console.log("Second play");
+ *       this._super();
+ *     }
+ *   });
+ *
+ *   (new Fab()).play(); // "Second play" then "First play" 
+ */
+Fab.extend = function extend(obj) {
+	// Loop through each property to extend
+	for (var prop in obj) {
+		// Override the prototype's value with the new value or a facade function if necessary
+		Fab.prototype[prop] = (typeof obj[prop] === "function" && rSuper.test(obj[prop])) ? createFacade(obj[prop], Fab.prototype[prop]) : obj[prop];
+	}
+}; // end of Fab.extend()
+
+
+// Extend the prototype
+Fab.extend({
 	/**
-	 * The ID attribute of the enhanced element
-	 * @type string
-	 */
-	id: null,
-
-	/**
-	 * The whole instance configuration
-	 * @type object
-	 */
-	_config: {},
-
-	/**
-	 * The enhanced element
-	 * @type Node
-	 */
-	element: null,
-
-	/**
-	 * The current renderer
-	 * @type Renderer
-	 */
-	_renderer: null,
-
-
-	/**
-	 * Create a new player instance using an existing &lt;video&gt; or &lt;audio&gt; tag. Allow to set a different configuration.
-	 * @contructs
+	 * Initialize an instance
+	 * This method exists so you can extend it and handle specific cases in your plugin.
+	 * Calling this method on an existing instance will reset it.
+	 * @api dev
 	 *
-	 * @param {string} id The ID attribute of the source &lt;video&gt; or &lt;audio&gt; to enhance
-	 * @param {object} [config={}] The player configuration
-	 *
-	 * @returns {fabuloos} Return a new player instance or false if the element doesn't exists
-	 *
-	 * @example
-	 *  <code>
-	 *    var player = fabuloos( "media" );
-	 *    var player = fabuloos( "media", { width: 720 } );
-	 *  </code>
+	 * @see #fab() for signatures
 	 */
-	init: function( id, config ) {
-		// Don't execute while extending
-		if (fab.extending) {
-			return;
+	init: function init(config) {
+		// We're trying to initialize an existing instance, destroy it first
+		if (this._ && this._.index !== undefined) {
+			this.destroy();
 		}
 
-		var
-			// Prepare a local config var to avoid overriding the received one
-			_config = {},
+		// Declare a private space (will reset the existing one if any)
+		this._ = {
+			// Define the index for this instance in the instances' cache
+			index: Fab.instances.push(this) - 1,
 
-			// Loop specific
-			property, value,
-			i = 0, count;
+			// Define the default supported renderers (may be updated when setting the config)
+			renderers: Renderer.supported
+		};
 
-		// Handle undefined config
-		config = config || {};
-
-		// Test the renderers
-		this.renderers( Renderer.supported );
-
-		// Copy the received config in the local _config
-		for (property in config) {
-			_config[property] = config[property];
-		}
-
-		// Store element's related stuff
-		this.id      = id;
-		this.element = document.getElementById( id );
+		// Try to find the element to base on
+		this.element(config);
 
 		// Use a closure to prepare a handleManager to correct the "this" keyword when receiving an event from a renderer
-		this.handleManager = (function( instance ){
+		/*this.handleManager = (function( instance ){
 			return function() {
 				return instance.trigger.apply( instance, arguments );
 			};
-		}( this ));
+		}( this ));*/
+
+		// Set the rest of the config (and allow chaining)
+		return this.set(config);
+	}, // end of init()
+
+
+	/**
+	 * Destroy the instance
+	 * Will restore the initial element and remove the instance from the cache.
+	 *
+	 * @param {undefined}
+	 * @return {null}
+	 */
+	destroy: function destroy() {
+		// Restore the old element (if any)
+		this.restore();
+
+		// Remove this instance from the caches
+		Fab.instances.splice(this._.index, 1);
+
+		// It is more convenient to return null (end chaining)
+		return null;
+	}, // end of destroy()
+
+
+	/**
+	 * Getter and setter for the current element
+	 * Get the element reflecting the player (depend on the renderer in use).
+	 * Set the element to replace with a player.
+	 *
+	 * @param {string} id The ID attribute of the element to enhance (might be `<audio>`, `<video>` or any element).
+	 * @return {fabuloos} Return the current instance to allow chaining.
+	 *
+	 * @param {Element} element The element to enhance (might be `<audio>`, `<video>` or any element).
+	 * @return {fabuloos} Return the current instance to allow chaining.
+	 *
+	 * @param {object} config The configuration to apply.
+	 *   To define which element to base on you must provide an `element` property.
+	 *   Its value can be the same as previous signatures (string or Element).
+	 * @return {fabuloos} Return the current instance to allow chaining.
+	 *
+	 * @param {undefined}
+	 * @return {Element|null} Return the current element reflecting the player.
+	 */
+	element: function element(config) {
+		// Act as getter if there is no arguments
+		if (config === undefined) {
+			return this._.element;
+		}
+
+		var
+			// @see #fab()
+			elt = config ? config.element || config : {},
+			id  = typeof elt === "string" ? elt.replace("#", "") : elt.id;
+
+		// If we are changing the element, restore the old one
+		this.restore();
+
+		this._.id  = id || this._.id; // Set the new id or use the default setted by #restore()
+		this._.old = this._.element = elt.nodeName ? elt : document.getElementById(id); // Set the current element
 
 		// An audio or video element was found, try to read its config
-		if (this.element && /audio|video/i.test( this.element.nodeName )) {
+		/*if (this.element && /audio|video/i.test( this.element.nodeName )) {
 			// Watch for defined attributes
 			for (count = this.element.attributes.length; i < count; i++) {
 				property = this.element.attributes[i].name;
@@ -146,115 +261,28 @@ fab.prototype = {
 					_config[property] = value;
 				}
 			}
+		}*/
+
+		return this; // Chaining
+	}, // end of element()
+
+
+	/**
+	 * Restore the initial element
+	 *
+	 * @param {undefined}
+	 * @return {fabuloos} Return the current instance to allow chaining.
+	 */
+	restore: function restore() {
+		// Replace the element with the old one if possible
+		if (this.element && this._.element !== this._.old) {
+			this._.element.parentNode.replaceChild(this._.old, this._.element);
 		}
 
-		// Finally, save the received config
-		this.config( _config );
-	} // end of init()
-};
+		// Set a default id since this instance isn't related to any element
+		this._.id  = "fabuloos-" + (Fab.instances.length + 1);
+		this._.old = this._.element = null;
 
-// Set the init's prototype to fabuloos' one to allow instanciation of fabuloos using init
-fab.prototype.init.prototype = fab.prototype;
-
-
-/**
- * Extend fabuloos with a given object literal. Simulating inheritance by giving access to _super.
- * @static @function
- * @see <a href="http://ejohn.org/blog/simple-javascript-inheritance/">John Resig - Simple JavaScript Inheritance</a>
- *
- * @param {object} obj The object literal containing the properties and methods to add to fabuloos
- *
- * @example
- *  <code>
- *    fabuloos.extend({
- *      play: function() {}
- *    });
- *  </code>
- */
-fab.extend = function( obj ) {
-	// We are extending the main class, prevent code execution on init when copying the prototype
-	fab.extending = true;
-
-	var
-		// Set a RegExp to test for _super calls in methods
-		fnTest = /xyz/.test( function() { "xyz"; } ) ? /\b_super\b/ : /.*/,
-
-		// Copy the current prototype to use it in _super magical methods
-		_super = this.prototype,
-
-		// Get the current instance state
-		prototype = new this.prototype.init(),
-
-		// Loop specific
-		prop,
-
-		// Define a function to create a function launching the extended method and giving access to _super
-		createExtended = function( name, fn, _super ) {
-			return function() {
-				var
-					ret, // The return value
-					tmp = this._super; // Store the current _super to avoid overwritting
-
-				// Give access to the _super method in the method
-				this._super = _super[name];
-
-				// Launch the function and store the result
-				ret = fn.apply( this, arguments );
-
-				// Restore the _super in case of bad overwritting (typically calling a different _super in a _super)
-				this._super = tmp;
-
-				return ret;
-			};
-		};
-
-	// Loop through each property to extend
-	for (prop in obj) {
-		// Override the property if the new property value is a function and already exists
-		prototype[prop] = (typeof obj[prop] === "function" && typeof _super[prop] === "function" && fnTest.test( obj[prop] )) ?
-			createExtended( prop, obj[prop], _super ) :
-			obj[prop]; // Otherwise, simply add the property/method
-	}
-
-	// Saving the brand new prototype over the class' one
-	this.prototype = prototype;
-
-	// Remember to redefined the link of the init's prototype to the class prototype to allow instanciation
-	this.prototype.init.prototype = this.prototype;
-
-	// We're not extending anymore
-	fab.extending = false;
-}; // end of extend()
-
-
-/**
- * Exception class
- * @constructor
- *
- * @params {number} code The error code to create
- *
- * @returns {Exception} A new Exception instance
- */
-function Exception( code ) {
-	this.name = "fabuloos error";
-	this.code = code;
-
-	// Loop through each property to find the static var related to this code
-	for (var prop in Exception) {
-		if (Exception[prop] === code) {
-			this.message = prop;
-		}
-	}
-} // end of Exception constructor
-
-Exception.prototype = new Error(); // Inherit from Error
-Exception.prototype.constructor = Exception; // Don't forget to correct the constructor
-
-// Mimic the DOMException error codes
-Exception.NOT_FOUND_ERR = 8;
-Exception.SYNTAX_ERR    = 12;
-
-
-// Expose
-fab.Exception  = Exception;
-window.fabuloos = window.fab = fab;
+		return this; // Chaining
+	} // end of restore()
+}); // end of Fab.extend()
